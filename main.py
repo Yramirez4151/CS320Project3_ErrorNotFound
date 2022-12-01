21909ea438dca1ccb44787811ec9bb283666ffc9
Yesenia Ramirez <ramirezy@lafayette.edu>
2022-12-01 11:15:15 -0500

    CONNECTION WORKING

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import psycopg2
 
 con = psycopg2.connect(
     port = "3200",
-    host="localhost",
+    host="139.147.200.35",
     database="template1",
     user="yesenia",
     password="")
 
 cur = con.cursor()
 
-cur.execute("CREATE TABLE Persons ( PersonID int, LastName varchar(255), FirstName varchar(255), Address varchar(255), City varchar(255));")
+# cur.execute("CREATE TABLE Persons ( PersonID int );")
 
-cur.execute("SELECT * FROM Persons;")
+# cur.execute("SELECT * FROM Persons;")
 
-cur.execute("INSERT INTO Persons VALUES ( 1, 'Huico', 'Josh', '1619 W North St', 'Chicago');")
+# cur.execute("INSERT INTO Persons VALUES ( 1 );")
 
-print(cur.execute("SELECT * FROM Persons;"))
+# print(cur.execute("SELECT * FROM Persons;"))
 
 print(con.closed)
 con.close()
