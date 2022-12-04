65ce6c8ede52ac194355e419cb83cb4a40875d0c
Yesenia Ramirez <ramirezy@lafayette.edu>
2022-12-04 14:37:51 -0500

    Create & Drop Function added

--- main.py
+++ main.py
@@ -29,39 +29,39 @@ def create_tables():
             vendor_name VARCHAR(255) NOT NULL
         )
         """,
-        # """ CREATE TABLE parts (
-        #         part_id SERIAL PRIMARY KEY,
-        #         part_name VARCHAR(255) NOT NULL
-        #         )
-        # """,
-        # """
-        # CREATE TABLE part_drawings (
-        #         part_id INTEGER PRIMARY KEY,
-        #         file_extension VARCHAR(5) NOT NULL,
-        #         drawing_data BYTEA NOT NULL,
-        #         FOREIGN KEY (part_id)
-        #         REFERENCES parts (part_id)
-        #         ON UPDATE CASCADE ON DELETE CASCADE
-        # )
-        # """,
-        # """
-        # CREATE TABLE vendor_parts (
-        #         vendor_id INTEGER NOT NULL,
-        #         part_id INTEGER NOT NULL,
-        #         PRIMARY KEY (vendor_id , part_id),
-        #         FOREIGN KEY (vendor_id)
-        #             REFERENCES vendors (vendor_id)
-        #             ON UPDATE CASCADE ON DELETE CASCADE,
-        #         FOREIGN KEY (part_id)
-        #             REFERENCES parts (part_id)
-        #             ON UPDATE CASCADE ON DELETE CASCADE
-        # )
-        # """
+        """ CREATE TABLE parts (
+                part_id SERIAL PRIMARY KEY,
+                part_name VARCHAR(255) NOT NULL
+                )
+        """,
+        """
+        CREATE TABLE part_drawings (
+                part_id INTEGER PRIMARY KEY,
+                file_extension VARCHAR(5) NOT NULL,
+                drawing_data BYTEA NOT NULL,
+                FOREIGN KEY (part_id)
+                REFERENCES parts (part_id)
+                ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        """,
+        """
+        CREATE TABLE vendor_parts (
+                vendor_id INTEGER NOT NULL,
+                part_id INTEGER NOT NULL,
+                PRIMARY KEY (vendor_id , part_id),
+                FOREIGN KEY (vendor_id)
+                    REFERENCES vendors (vendor_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE,
+                FOREIGN KEY (part_id)
+                    REFERENCES parts (part_id)
+                    ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        """
         )
     #conn = None
     conn = psycopg2.connect(
             port = "3200",
-            host="139.147.200.35",
+            host="139.147.207.184",
             database="template1",
             user="yesenia",
             password="")
@@ -83,26 +83,45 @@ def create_tables():
         if conn is not None:
             conn.close()
 
-def delete_tables():
-    tableName = "vendor_parts"
+def delete_tables(tableNames):
+    conn = psycopg2.connect(
+            port = "3200",
+            host="139.147.207.184",
+            database="template1",
+            user="yesenia",
+            password="")
+    
+    cur = conn.cursor()
+    for x in tableNames:
+        #tableName = x
+        dropTableStmt = "DROP TABLE %s CASCADE;" %x
+        cur.execute(dropTableStmt);
+        conn.commit()
+
+    cur.close()
+    conn.close()
+
+def delete_table():
+    tableName = "StudentInfo"
     dropTableStmt = "DROP TABLE %s;" %tableName
     conn = psycopg2.connect(
             port = "3200",
-            host="139.147.200.35",
+            host="139.147.207.184",
             database="template1",
             user="yesenia",
             password="")
 
     cur = conn.cursor()
-    cur.execute(dropTableStmt);
+    cur.execute(dropTableStmt)
 
     cur.close()
     conn.commit()
     conn.close()
 
 if __name__ == '__main__':
-    #create_tables()
-    delete_tables()
+    tableNames = [ "vendors", "part_drawings", "parts", "vendor_parts"]
+    create_tables() 
+    delete_tables(tableNames)
 
 
 #print(conn.closed)
