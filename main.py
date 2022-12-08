3cb0a3c16d2c06fe1a6904a5c2a3b2d74fb3832b
Yesenia Ramirez <ramirezy@lafayette.edu>
2022-12-08 12:18:15 -0500

    Insertion working -JoshuaG

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def create_tables():
             FName VARCHAR(50) NOT NULL,
             MName VARCHAR(50),
             LName VARCHAR(50) NOT NULL,
-            ClassTitle VARCHAR(7),
+            ClassTitle VARCHAR(10),
             GradYear INTEGER NOT NULL,
-            Email VARCHAR(50) NOT NULL,
+            Email VARCHAR(200) NOT NULL,
             PhoneNum INTEGER, 
             Address VARCHAR(50) NOT NULL,
             ResidenceHallID INTEGER,
@@ -136,7 +136,7 @@ def create_tables():
     #conn = None
     conn = psycopg2.connect(
             port = "3200",
-            host="139.147.224.54",
+            host="139.147.199.45",
             database="template1",
             user="yesenia",
             password="")
@@ -161,7 +161,7 @@ def create_tables():
 def delete_tables(tableNames):
     conn = psycopg2.connect(
             port = "3200",
-            host="139.147.224.54",
+            host="139.147.199.45",
             database="template1",
             user="yesenia",
             password="")
@@ -181,7 +181,7 @@ def delete_table():
     dropTableStmt = "DROP TABLE %s;" %tableName
     conn = psycopg2.connect(
             port = "3200",
-            host="139.147.224.54",
+            host="139.147.199.45",
             database="template1",
             user="yesenia",
             password="")
@@ -193,7 +193,31 @@ def delete_table():
     conn.commit()
     conn.close()
 
+def insert():
+    try:
+        conn = psycopg2.connect(
+                port = "3200",
+                host="139.147.199.45",
+                database="template1",
+                user="yesenia",
+                password="")
+
+        cur = conn.cursor()
+        postgres_insert_query = """ INSERT INTO StudentInfo (LNum, FName, MName, LName, ClassTitle, GradYear, Email, PhoneNum, Address, ResidenceHallID, RoomID, DOB, Enrollmentstatus, POBox, FERPA, SSN, PreferredName, MealPlan, Notes, EmployeeID, ExcelStudent) VALUES (%s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s, %s)"""
+        record_to_insert = (989815820,'Doina', 'Jakub', 'Andrews', 'Freshman', 2032, 'jakubd@lafayette.edu', '412-734-7650','1606 West Drive', 1, 23, '1961-12-09', 'Active', 9965, True, 500089487, 'Yessi', 7,'', 1, False)
+        cur.execute(postgres_insert_query, record_to_insert)
+
+        conn.commit()
+        count = cur.rowcount
+        print(count, "Record inserted successfully into vendor")
+    except(Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
 if __name__ == '__main__':
     tableNames = [ "StudentInfo", "Degree", "DegreeRequirements", "Course", "Product", "CoursesTaken", "Staff", "Contacts", "FinancialAid"]
     #create_tables() 
-    delete_tables(tableNames)
+    #delete_tables(tableNames)
+    #insert()
